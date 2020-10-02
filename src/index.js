ec399697779305cb150a9c2160048662defe2c32
Anastasiya <anastasiaasn@gmail.com>
2020-10-02 11:28:46 +0300

    feat: add realization of Human readable function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    const numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    const hundred = ' hundred ';
+
+    let result = "";
+
+    if (number === 0) return numbers[0];
+    if (number >= 100) {
+        result += numbers[Math.trunc(number / 100)] + hundred;
+    }
+    if (number % 100 < 20 && number % 100 > 0) {
+        result += numbers[Math.trunc(number % 100)];
+    } else if (number % 100 >= 20) {
+        result += tens[Math.trunc((number % 100) / 10) - 2]
+        if (number % 10) {
+            result += " " + numbers[Math.trunc(number % 10)]
+        }
+    }
+
+    return result.trim();
 }
